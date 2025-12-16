62c5ce6503137ffdf64d3c43f38567ca521a6a0e
nirvair09 <chandradeep262003@gmail.com>
2025-12-16 12:24:20 +0530

    feat: Add cart context with AsyncStorage persistence and related dependency.

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,13 +1,21 @@
-import { createContext, useReducer } from "react";
-
-console.log('CartContext - useReducer defined:', !!useReducer);
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useEffect, useReducer } from "react";
+// console.log('CartContext - useReducer defined:', !!useReducer);
 
 
 export const CartContext = createContext();
 
+const STORAGE_KEY = "cart";
+
 function cartReducer(state, action) {
 
+
     switch (action.type) {
+
+        case 'SET_CART': {
+            return action.payload;
+        }
+
         case 'ADD_TO_CART': {
             const existing = state.find(
                 (item) => item.id === action.product.id
@@ -55,7 +63,41 @@ function cartReducer(state, action) {
 
 export function CartProvider({ children }) {
     console.log('CartProvider rendering');
-    const [cart, dispatch] = useReducer(cartReducer, [])
+    const [cart, dispatch] = useReducer(cartReducer, []);
+
+    useEffect(() => {
+        async function loadCart() {
+            try {
+                const storedData = await AsyncStorage.getItem(STORAGE_KEY);
+                if (storedData) {
+                    dispatch({
+                        type: "SET_CART",
+                        payload: JSON.parse(storedData),
+                    });
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        loadCart();
+    }, []);
+
+
+    useEffect(() => {
+        async function saveCart() {
+            try {
+                await AsyncStorage.setItem(
+                    STORAGE_KEY,
+                    JSON.stringify(cart)
+                );
+            } catch (error) {
+                console.log("Failed to save cart", error);
+            }
+        };
+
+        saveCart();
+    }, [cart]);
 
     return (
         <CartContext.Provider value={{ cart, dispatch }}>
